c29b3aef5d1fa809de1dde60b140b82e45fe24d5
LDMRTN <liam.martin.491@gmail.com>
2025-02-02 11:23:55 -0600

    First commit

---
+++ newGhRepo.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+repoName=$1
+gitUserName=$2
+
+while [ -z "$repoName" ]
+do
+   echo 'Provide a repository name'
+   read -r -p $'Repository name:' repoName
+done
+
+echo "# $repoName" >> README.md
+git init
+git add .
+git commit -m "First commit"
+
+curl -u "$gitUserName" https://api.github.com/user/repos -d '{"name": "'"$repoName"'", "private":false}'
+
+GIT_URL=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$gitUserName"/"$repoName" | jq -r '.clone_url')
+
+git branch -M main
+git remote add origin $GIT_URL
+git push -u origin main
